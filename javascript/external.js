e7474ca91e13ece959728f1881ed6b775edbc03f
hsp31 <hsp31@txstate.edu>
2026-09-22 03:27:44 -0500

    Defined a function for the welcome message.

--- javascript/external.js
+++ javascript/external.js
@@ -1,1 +1,11 @@
-let user_name = window.prompt("Please enter your name", "Type your name here");
+// let user_name = window.prompt("Please enter your name", "Type your name here");
+function welcome()
+{
+  let a = "Please enter your name.";
+  let b = "Type your name here.";
+
+  user_name = window.prompt(a, b);
+  message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1"
+  return message
+}
+document.write(welcome());
